06edd5fb0bc33d5787bcca67d5bf0a90f8240558
aurelflip <82026487+aurelflip@users.noreply.github.com>
2021-05-04 10:34:02 +0200

    update self .all

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -11,6 +11,13 @@ class Gossip
       csv << [@content, @author]
     end
   end
+  def self.all
+  all_gossips = []
+  CSV.read("./db/gossip.csv").each do |csv_line|
+    all_gossips << Gossip.new(csv_line[0], csv_line[1])
+  end
+  return all_gossips
+end
 end
   
 
